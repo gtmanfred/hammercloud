43227fdea4af30207a8c35a17df9d95b9773e170
William Giokas <1007380@gmail.com>
2016-04-22 22:49:35 -0500

    Fix the colors and static importing
    Alright, so python is weird. If something is defined in __init__.py, it
    will be run when the module itself is imported. That's fine. However, it
    will also run when I import one if it's sub-modules, that is also run. I
    have removed the imports from the static __init__.py file, so that we
    can safely import `hammercloud.static.const` without running into issues
    in the other static things we don't care about.

--- hammercloud/static/__init__.py
+++ hammercloud/static/__init__.py
@@ -1,7 +1,3 @@
 from __future__ import absolute_import
-from hammercloud.static.account import Account
-from hammercloud.static.server import Server
-from hammercloud.static.shell import Shell
-from hammercloud.static import const
 
 __all__ = ['Account', 'Server']
